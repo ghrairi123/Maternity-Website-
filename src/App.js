be8a3a948974bbe3258216a74e0105e82ce060cc
unknown <ghrairimarwa54@gmail.com>
2022-10-29 11:33:38 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./Component/Public/Footer";
+import Header from "./Component/Public/Header";
+import Home from "./Component/Public/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Home />
+      <Footer />
     </div>
   );
 }
